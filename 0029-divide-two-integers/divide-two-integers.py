caf7f342c0a887b0f6904552bc8ee1ef3eff9888
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-03-22 16:57:58 -0500

    Time: 0 ms (100.00%) Space: 17.9 MB (23.06%)

--- 0029-divide-two-integers/divide-two-integers.py
+++ 0029-divide-two-integers/divide-two-integers.py
@@ -1,45 +1,36 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
 
-        # Constants.
-        MAX_INT = 2147483647  # 2**31 - 1
-        MIN_INT = -2147483648  # -2**31
-        HALF_MIN_INT = -1073741824  # MIN_INT // 2
+        int_max = 2**31-1
+        int_min = -2**31
+        half = int_min//2
 
-        # Special case: overflow.
-        if dividend == MIN_INT and divisor == -1:
-            return MAX_INT
+        if dividend == int_min and divisor == -1:
+            return int_max
 
-        # We need to convert both numbers to negatives.
-        # Also, we count the number of negatives signs.
-        negatives = 2
-        if dividend > 0:
-            negatives -= 1
-            dividend = -dividend
-        if divisor > 0:
-            negatives -= 1
-            divisor = -divisor
+        neg_result = True if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0) else False
+
+
+        dividend = -abs(dividend)
+        divisor = -abs(divisor)
 
         quotient = 0
-        # Once the divisor is bigger than the current dividend,
-        # we can't fit any more copies of the divisor into it anymore */
-        while divisor >= dividend:
-            # We know it'll fit at least once as divivend >= divisor.
-            # Note: We use a negative powerOfTwo as it's possible we might have
-            # the case divide(INT_MIN, -1). */
-            powerOfTwo = -1
-            value = divisor
-            # Check if double the current value is too big. If not, continue doubling.
-            # If it is too big, stop doubling and continue with the next step */
-            while value >= HALF_MIN_INT and value + value >= dividend:
-                value += value
-                powerOfTwo += powerOfTwo
-            # We have been able to subtract divisor another powerOfTwo times.
-            quotient += powerOfTwo
-            # Remove value so far so that we can continue the process with remainder.
-            dividend -= value
 
-        # If there was originally one negative sign, then
-        # the quotient remains negative. Otherwise, switch
-        # it to positive.
-        return -quotient if negatives != 1 else quotient
+        while divisor >= dividend:
+
+            powers = -1
+            curr_divisor = divisor
+            while curr_divisor >= half and curr_divisor * 2 >= dividend:
+                print(curr_divisor)
+                powers *= 2
+                curr_divisor *= 2
+            
+            quotient += powers
+            dividend -= curr_divisor
+        
+        return quotient if neg_result else -quotient
+        
+
+
+
+        
