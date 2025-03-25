b8df401193fc64ab6c3567ce31aa4a45b27221b8
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-03-25 14:04:40 -0500

    Time: 599 ms (82.93%) Space: 18.2 MB (29.27%)

--- 2209-number-of-equal-count-substrings/number-of-equal-count-substrings.py
+++ 2209-number-of-equal-count-substrings/number-of-equal-count-substrings.py
@@ -2,40 +2,31 @@ class Solution:
     def equalCountSubstrings(self, s: str, count: int) -> int:
 
         ans = 0
-        size = count
 
-        def check(freq):
-            return all([v == count or v == 0 for k, v in freq.items()])
-
-        while size <= len(s) and size <= 26 * count:
-            freqs = {}
-
-            # set up
-            for i in range(size):
+        for k in range(1, 27):
+            # for all possible unique character counts
+            # we are trying to find substrings with k unique characters of freq == count
+            freqs = [0] * 26
+            unique_cnt = 0
+            for i in range(len(s)):
+                length = k * count  # length of the substrings we are considering
                 curr = ord(s[i]) - ord('a')
-                if curr not in freqs:
-                    freqs[curr] = 0
                 freqs[curr] += 1
-            
-            # first check
-            if check(freqs):
-                ans += 1
-
-            
-            for i in range(len(s) - size):
-                curr = ord(s[i]) - ord('a')
-                freqs[curr] -= 1
-                if freqs[curr] == 0:
-                    del freqs[curr]
-                next_char = ord(s[i + size]) - ord('a')
-                if next_char not in freqs:
-                    freqs[next_char] = 0
-                freqs[next_char] += 1
+                if freqs[curr] == count:
+                    unique_cnt += 1
                 
-                if check(freqs):
+                # move the window
+                if i >= length:
+                    char_curr = ord(s[i - length]) - ord('a')
+                    if freqs[char_curr] == count:
+                        unique_cnt -= 1
+                    freqs[char_curr] -= 1
+                
+                # if the unique amt of characters inside a certain window = k
+                # we add 1 to answer
+                if unique_cnt == k:
                     ans += 1
-
-            size += count
         return ans
-
+                
+        
         
