ad5763d7e526ac1ee931427964aafbda5f7d4bfe
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-03-29 00:11:14 -0500

    Time: 1 ms (96.78%) Space: 19.3 MB (57.41%)

---
+++ 0813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -0,0 +1,21 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        ans = []
+
+        def dfs(curr, arr):
+            if curr == len(graph) - 1:
+                ans.append(arr[:])
+                return
+
+            for n in graph[curr]:
+                arr.append(n)
+                dfs(n, arr)
+                arr.pop()
+            
+            
+        
+        dfs(0, [0])
+        return ans
+        
+        
