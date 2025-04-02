ab8e881e9fd4e65c5bf422c48fd280dcfa827a68
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-04-02 13:43:51 -0500

    Time: 131 ms (64.25%) Space: 30.2 MB (87.99%)

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -1,19 +1,15 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-        score = weights[0] + weights[-1]
-        costs_min = []
-        costs_max = []
-
+        if k == 1:
+            return 0
+        
+        pair_sums = []
         for i in range(len(weights) - 1):
-            curr_cost = weights[i] + weights[i + 1]
-            heapq.heappush(costs_min, -curr_cost)
-            heapq.heappush(costs_max, curr_cost)
-            if len(costs_min) > k - 1:
-                heapq.heappop(costs_min)
-            if len(costs_max) > k - 1:
-                heapq.heappop(costs_max)
+            pair_sums.append(weights[i] + weights[i + 1])
         
-        return (sum(costs_max)+ score) - (abs(sum(costs_min)) + score)
-            
+        pair_sums.sort()
         
+        min_score = sum(pair_sums[:k-1])
+        max_score = sum(pair_sums[-(k-1):])
         
+        return max_score - min_score
