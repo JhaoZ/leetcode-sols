4377af74a781cb77a998b9fc6aee9ac9e097c6da
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-04-11 12:53:30 -0500

    Time: 466 ms (7.45%) Space: 43.6 MB (6.38%)

---
+++ 2457-count-special-integers/count-special-integers.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countSpecialNumbers(self, n: int) -> int:
+        
+        n = str(n)
+
+        @cache
+        def dp(pos, smaller, distinct, started, mask):
+            if pos >= len(n):
+                return int(distinct and started)
+            
+            limit = 9 if smaller else int(n[pos])
+
+            ans = 0
+
+            for i in range(limit + 1):
+                repeat = bool(mask & (1 << i))
+                next_smaller = smaller or ((not smaller) and (i < int(n[pos])))
+                next_started = started or (i != 0)
+                next_mask = mask | (1 << i)
+
+                if not started and i == 0:
+                    repeat = False
+                    next_mask = mask
+                
+                ans += dp(pos + 1, next_smaller, (not repeat) and distinct, next_started, next_mask)
+            
+            return ans
+        
+        return dp(0, False, True, False, 0)
+                
+
+                
+                
+
+
+
