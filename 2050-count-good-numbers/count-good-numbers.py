39fd62e114255617e838468ec4a2a673345c0b15
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-04-12 22:14:38 -0500

    Time: 0 ms (100.00%) Space: 17.8 MB (44.07%)

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,15 +1,4 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         mod = 10**9 + 7
-
-        # use fast exponentiation to calculate x^y % mod
-        def quickmul(x: int, y: int) -> int:
-            ret, mul = 1, x
-            while y > 0:
-                if y % 2 == 1:
-                    ret = ret * mul % mod
-                mul = mul * mul % mod
-                y //= 2
-            return ret
-
-        return quickmul(5, (n + 1) // 2) * quickmul(4, n // 2) % mod
+        return pow(5, (n + 1) // 2, mod) * pow(4, n // 2, mod) % mod
