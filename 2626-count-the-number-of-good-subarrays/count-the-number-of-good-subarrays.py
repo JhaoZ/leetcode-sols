3712e98f8a157ff5917aa47139a6b01df9aecb18
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-04-15 23:48:01 -0500

    Time: 248 ms (5.83%) Space: 34.5 MB (16.83%)

---
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        ans = 0
+        count = 0
+        freq = defaultdict(int)
+        end = 0
+        for start in range(len(nums)):
+            while end < len(nums) and count < k:
+                count += freq[nums[end]] 
+                freq[nums[end]] += 1
+                end += 1
+            
+            if count >= k:
+                print(end)
+                ans += len(nums) - (end - 1)
+
+            
+            freq[nums[start]] -= 1
+            count -= freq[nums[start]]
+        return ans
+            
+
+
+
+        
