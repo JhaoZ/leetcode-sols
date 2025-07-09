baad7ea0d5df56e7a73239760e25194d5e2b533c
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-07-08 20:07:42 -0700

    Time: 198 ms (5.32%) Space: 44.6 MB (5.32%)

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        empty_spots = [0]
+        events = [[startTime[i], endTime[i]] for i in range(len(startTime))]
+        events.sort()
+        
+        print(events)
+
+        prev_time = 0
+        for s, e in events:
+            gap = s - prev_time
+            empty_spots.append(empty_spots[-1] + gap)
+            prev_time = e
+        if eventTime - prev_time:
+            print(eventTime - prev_time)
+            empty_spots.append(empty_spots[-1] + (eventTime - prev_time))
+        
+        if k >= len(empty_spots) - 1:
+            return empty_spots[-1]
+
+        ans = 0
+        for i in range(k + 1, len(empty_spots)):
+            ans = max(ans, empty_spots[i] - empty_spots[i - k - 1])
+        
+
+
+        return ans
