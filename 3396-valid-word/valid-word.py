4454ede3ca3610e94039fbc62f178aa8ad3e6236
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-07-14 20:44:21 -0700

    Time: 35 ms (1.79%) Space: 17.7 MB (86.06%)

--- 3396-valid-word/valid-word.py
+++ 3396-valid-word/valid-word.py
@@ -2,6 +2,7 @@ class Solution:
     def isValid(self, word: str) -> bool:
         if len(word) < 3:
             return False
+            
         vowels = set(['a', 'e', 'i', 'o', 'u'])
         t = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
         digits = set(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
