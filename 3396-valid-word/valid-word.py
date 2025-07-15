457c99bb5a851b8924a71699021819c472a7872d
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-07-14 20:44:41 -0700

    Time: 39 ms (1.79%) Space: 17.8 MB (34.46%)

--- 3396-valid-word/valid-word.py
+++ 3396-valid-word/valid-word.py
@@ -6,9 +6,10 @@ class Solution:
         vowels = set(['a', 'e', 'i', 'o', 'u'])
         t = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
         digits = set(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
-        
-        v = False
         c = False
+        v = False
+        
+
         for i in word:
             if i.lower() not in t:
                 return False
