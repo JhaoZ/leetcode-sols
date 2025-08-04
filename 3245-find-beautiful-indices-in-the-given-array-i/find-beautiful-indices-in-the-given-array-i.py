bd030dea8897b87e3ce93b7b0412798cab06dd46
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-08-03 21:37:27 -0700

    Time: 115 ms (63.77%) Space: 23.4 MB (8.12%)

---
+++ 3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+        a_idx = [i for i in range(len(s) - len(a) + 1) if s[i:i+len(a)] == a]
+        b_idx = [i for i in range(len(s) - len(b) + 1) if s[i:i+len(b)] == b]
+        ans = []
+        print(a_idx)
+        print(b_idx)
+        if not b_idx or not a_idx:
+            return ans
+        for idx in a_idx:
+            b_itr = bisect.bisect_right(b_idx, idx)
+           
+            if b_itr == len(b_idx):
+                if abs(b_idx[b_itr - 1] - idx) <= k:
+                    ans.append(idx)
+            else:
+                if abs(b_idx[b_itr] - idx) <= k or abs(b_idx[b_itr - 1] - idx) <= k:
+                    ans.append(idx)
+        return ans
