82b2a612e23ef60891763d29769f7d16a791112f
Bill Zhang <33736335+JhaoZ@users.noreply.github.com>
2025-08-20 13:05:51 -0400

    Time: 2 ms (37.52%) Space: 17.7 MB (92.60%)

---
+++ 0752-ip-to-cidr/ip-to-cidr.py
@@ -0,0 +1,33 @@
+class Solution:
+    def ipToCIDR(self, ip: str, n: int) -> List[str]:
+        ans = []
+
+        curr_ip = 0
+        for num in ip.split("."):
+            curr_ip = curr_ip * 256 + int(num)
+
+        def ip_to_str(ip_addr):
+            return ".".join(str((ip_addr >> (8 * i)) & 255) for i in range(3, -1, -1))
+
+       
+
+        while n > 0:
+            # fill next block
+            max_range = curr_ip & -curr_ip   # alignment constraint
+
+            if max_range == 0:   # special case for 0.0.0.0
+                max_range = 1 << 32
+            while max_range > n:             # don’t exceed what’s left
+                max_range //= 2
+            block_size = max_range
+
+            block_size = min(n, max_range)
+            
+            mask_len = 32 - (block_size.bit_length() - 1)
+
+           
+            ans.append(f'{ip_to_str(curr_ip)}/{mask_len}')
+
+            n -= block_size
+            curr_ip += block_size
+        return ans
